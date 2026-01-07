aaa1e6731f434b3363aa221ed4d94cf99f4947c8
RoseDeJuin <rozenn.gc@gmail.com>
2026-01-07 13:55:52 +0100

    Ajout de la tâche creer pour créer une nouvelle tâche.

--- src/application/services/TacheService.java
+++ src/application/services/TacheService.java
@@ -1,11 +1,16 @@
 package application.services;
 
+import application.domaine.Etat;
+import application.domaine.Tache;
 import application.repositories.TacheRepository;
+
+import java.util.List;
 
 public class TacheService {
 
 	private int compteurId = 11;
 	private final TacheRepository tacheRepository;
+	private List<Tache> listeTaches;
 
 	public TacheService() {
 		tacheRepository = new TacheRepository();
@@ -18,4 +23,14 @@ public class TacheService {
 	public TacheRepository getTacheRepository() {
 		return tacheRepository;
 	}
+
+	public void creer(String titre, String description){
+
+		Tache nouvTache = new Tache(compteurId + 1, titre, Etat.NON_ASSIGNEE, null);
+		listeTaches.add(nouvTache);
+		IO.println(description);
+		compteurId += 1;
+
+	}
+
 }
