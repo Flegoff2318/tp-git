867b12de9f76a6724baae70c78335fcd90d3377a
francois.le.goff <flegoff.dev@gmail.com>
2026-01-07 10:55:04 +0100

    Ajout méthodes `getUtilisateurs` et `creerUtilisateur` pour créer un utilisateur et l'ajouter à la liste d'utilisateurs existante.

--- src/application/services/UtilisateurService.java
+++ src/application/services/UtilisateurService.java
@@ -1,12 +1,20 @@
 package application.services;
 
+import application.domaine.Utilisateur;
 import application.repositories.UtilisateurRepository;
 
 public class UtilisateurService {
 
-	private final UtilisateurRepository utilisateurRepository;
+    private final UtilisateurRepository utilisateurRepository;
 
-	public UtilisateurService() {
-		utilisateurRepository = new UtilisateurRepository();
-	}
+    public UtilisateurService() {
+        utilisateurRepository = new UtilisateurRepository();
+    }
+
+    public Utilisateur creerUtilisateur(String prenom, String nom) {
+        String identifiantUtilisateur = prenom.charAt(0) + nom;
+        Utilisateur nouvelUtilisateur = new Utilisateur(identifiantUtilisateur, nom, prenom, "");
+        utilisateurRepository.getUtilisateurs().add(nouvelUtilisateur);
+        return nouvelUtilisateur;
+    }
 }
